53f09ca6da7ddb11e009e8c3516e2941f75949ed
NithuNIthisha <nithisha2201@gmail.com>
2023-04-30 05:27:29 +0530

    first commit

---
+++ border/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import "./border.css"
+import Border from "./Border"
+
+function App() {
+  return (
+    <div className="App">
+      <Border></Border>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ border/src/Border.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Border = () => {
+  return (
+    <div class="container">
+  <div class="semicircle"></div>
+  <div className='circle'></div>
+</div>
+  )
+}
+
+export default Border
